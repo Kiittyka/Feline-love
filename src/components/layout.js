627125d34ed45550f2271146d9a4410ab8285ceb
Kiittyka <krithikaudupa@gmail.com>
2020-02-27 15:14:51 +0530

    Added title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import { Helmet } from "react-helmet"
 import layoutStyles from './layout.module.scss'
 import "../styles/index.scss"
 
 const Layout = (props) => {
     return (
         <div className={layoutStyles.container}>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Feline Love</title>
+            </Helmet>
             <div className={layoutStyles.content}>
                 <Header />
                 {props.children}
