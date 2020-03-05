36ce2f532183c1c98c376639c41c846ab5226517
Kiittyka <krithikaudupa@gmail.com>
2020-03-05 14:42:12 +0530

    Removed home

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Header = () => {
             <h1><Link to="/" className={headerStyles.title}>Feline Love</Link></h1>
             <nav>
                 <ul className={headerStyles.navlist}>
-                    <li><Link className={headerStyles.navitem} to="/">Home</Link></li>
+                    {/* <li><Link className={headerStyles.navitem} to="/">Home</Link></li> */}
                     <li><Link className={headerStyles.navitem} to="/facts">Cat Facts</Link></li>
                     <li><Link className={headerStyles.navitem} to="/pics">Cat Pics</Link></li>
                 </ul>
